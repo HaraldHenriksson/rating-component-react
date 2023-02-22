c6af1b4e0a2afdae88760821184bb94bd337c80a
Harald <Harald@espinagar.se>
2023-02-22 17:28:15 +0100

    thank you panel done

--- vite-number-conversion/src/components/Rating.tsx
+++ vite-number-conversion/src/components/Rating.tsx
@@ -3,15 +3,34 @@ import styles from "./Rating.module.css";
 
 export function Rating() {
   const [selectedRating, setSelectedRating] = useState<number>();
-  const [isSubmitted, serIsSubmitted] = useState(false);
+  const [isSubmitted, setIsSubmitted] = useState(false);
 
   function handleRatingClicked(rating: number) {
     setSelectedRating(rating);
   }
 
-  return (
-    <form className={styles.panel}>
+  function handleFormSubmitted(e: React.FormEvent) {
+    e.preventDefault();
+    setIsSubmitted(true);
+  }
+
+  return isSubmitted ? (
+    <div className={styles.thankYouPanel}>
+      <img src="../illustration-thank-you.svg" alt="" />
+
+      <p className={styles.selected}>You selected {selectedRating} out of 5</p>
+
+      <h1 className={styles.title}>Thank you!</h1>
+
+      <p className={styles.discription}>
+        We appreciate you taking the time to give a rating. If you ever need
+        more support, don't hesitate to get in touch!
+      </p>
+    </div>
+  ) : (
+    <form onSubmit={(e) => setIsSubmitted(true)} className={styles.panel}>
       <img className={styles.star} src="../icon-star.svg" />
+
       <h1 className={styles.title}>How did we do?</h1>
 
       <p className={styles.discription}>
@@ -22,6 +41,7 @@ export function Rating() {
       <div className={styles.group}>
         {[1, 2, 3, 4, 5].map((rating) => (
           <button
+            type="button"
             onClick={() => handleRatingClicked(rating)}
             className={styles.rating}
           >
